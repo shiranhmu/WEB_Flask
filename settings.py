6b234aed38978ce858f143fcce0a936b15a13d5e
shiranhmu <shiranhmu@gmail.com>
2022-01-12 20:06:16 +0200

    ex11

--- settings.py
+++ settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # DB base configuration from .env for modularity and security reasons
 DB = {
-    'host' : os.environ.get('DB_HOST'),
+    'host': os.environ.get('DB_HOST'),
     'user': os.environ.get('DB_USER'),
     'password': os.environ.get('DB_PASSWORD'),
     'database': os.environ.get('DB_NAME')
